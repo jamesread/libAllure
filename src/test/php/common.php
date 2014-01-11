b3b481a53f4e14b214da702474afeb6ef7f8fb35
jamesread <jread@redhat.com>
2014-01-11 03:07:47 +0000

    Added debug print of include path

--- src/test/php/common.php
+++ src/test/php/common.php
@@ -4,4 +4,6 @@ date_default_timezone_set('Europe/London');
 $dir = realpath(dirname(__FILE__) . '/../main/php/');
 set_include_path(get_include_path() . PATH_SEPARATOR . "../main/php/");
 
+echo 'Include path for phpunit tests: ' . var_dump($dir);
+
 ?>
