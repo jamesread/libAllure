3997f5f100e79bfd64f940449f0662c394af41a0
jamesread <jread@redhat.com>
2014-01-11 03:19:01 +0000

    I should try writing code with my eyes actually open.

--- src/test/php/common.php
+++ src/test/php/common.php
@@ -1,10 +1,8 @@
 <?php
 
 date_default_timezone_set('Europe/London');
-$dir = realpath(dirname(__FILE__) . '/../main/php/';
-$travisPath = . '/home/travis/build/jamesread/libAllure/src/main/php/');
+$dir = realpath(dirname(__FILE__) . '/../main/php/');
+$travisPath = '/home/travis/build/jamesread/libAllure/src/main/php/';
 set_include_path(get_include_path() . PATH_SEPARATOR . "../main/php/" . PATH_SEPARATOR . $travisPath);
-
-echo 'Include path for phpunit tests: ' . var_dump($dir) . ' and I am in ' . __DIR__ . ' and my cwd is ' . getcwd();
 
 ?>
