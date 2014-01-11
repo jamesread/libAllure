2799a153b6040d5c91e0f2053b656e4f683b084e
jamesread <jread@redhat.com>
2014-01-11 03:14:21 +0000

    That's gotta fix it.

--- src/test/php/common.php
+++ src/test/php/common.php
@@ -1,9 +1,9 @@
 <?php
 
 date_default_timezone_set('Europe/London');
-$dir = realpath(dirname(__FILE__) . '/../main/php/');
+$dir = realpath(dirname(__FILE__) . '/../main/php/' . '/home/travis/build/jamesread/libAllure/src/main/php/');
 set_include_path(get_include_path() . PATH_SEPARATOR . "../main/php/");
 
-echo 'Include path for phpunit tests: ' . var_dump($dir) . ' and I am in ' __DIR__ . ' and my cwd is ' . getcwd();
+echo 'Include path for phpunit tests: ' . var_dump($dir) . ' and I am in ' . __DIR__ . ' and my cwd is ' . getcwd();
 
 ?>
