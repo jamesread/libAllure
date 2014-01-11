fe29426e98bdde6d6c974ed3bbc03fe82970d7d1
jamesread <jread@redhat.com>
2014-01-11 03:10:17 +0000

    More verbose pwd message

--- src/test/php/common.php
+++ src/test/php/common.php
@@ -4,6 +4,6 @@ date_default_timezone_set('Europe/London');
 $dir = realpath(dirname(__FILE__) . '/../main/php/');
 set_include_path(get_include_path() . PATH_SEPARATOR . "../main/php/");
 
-echo 'Include path for phpunit tests: ' . var_dump($dir);
+echo 'Include path for phpunit tests: ' . var_dump($dir) . ' and I am in ' __DIR__ . ' and my cwd is ' . getcwd();
 
 ?>
