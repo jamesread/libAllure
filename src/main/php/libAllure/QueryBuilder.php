1428d8e2d478f012338af98d48bafee1403adea6
jamesread <contact@jwread.com>
2016-04-19 14:27:58 +0100

    QueryBuilder.

--- src/main/php/libAllure/QueryBuilder.php
+++ src/main/php/libAllure/QueryBuilder.php
@@ -3,10 +3,16 @@
 namespace libAllure;
 
 class QueryBuilder {
-	private $_fields = array();
+	private $fields = array();
 	private $verb;
 	private $from;
 	private $orderBy = array();
+	private $where = array();
+	private $joins = array();
+	private $joinConditions = array();
+
+	private $lastPrefix = null;
+	private $lastJoinedTabke = null;
 
 	public function __construct($verb = 'SELECT') {
 		$verb = strtoupper($verb);
@@ -16,40 +22,222 @@ class QueryBuilder {
 
 	public function orderBy() {
 		foreach (func_get_args() as $arg) {
+			$arg = $this->addFieldPrefix($arg);
+
 			array_push($this->orderBy, $arg);
 		}
 
 		return $this;
 	}
 
-	public function from($from) {
-		$this->from = $from;
+	public function from($from, $prefix = null) {
+		if ($prefix == null) {
+			$prefix = substr($from, 0, 1);
+		}
+
+		$this->from = array( 
+			"prefix" => $prefix,
+			"table" => $from
+		);
+
+		$this->lastPrefix = $prefix;
 
 		return $this;
 	}
 
 	public function fields() {
 		foreach (func_get_args() as $arg) {
-			array_push($this->_fields, $arg);
+			if (is_array($arg)) {
+				$field = array(
+					'field' => $arg[0],
+					'alias' => $arg[1]
+				);
+			} else {
+				$field = array(
+					'field' => $arg,
+					'alias' => null
+				);
+			}
+
+			$field['field'] = $this->addFieldPrefix($field['field']);
+		
+			array_push($this->fields, $field);
 		}
 
 		return $this;
 	}
 
+	protected function addFieldPrefix($field) {
+		if (strpos($field, '!') !== FALSE) {
+			return str_replace('!', '', $field);
+		}
+
+		if (strpos($field, '.') === FALSE) {
+			$prefix = $this->lastPrefix . '.';
+		} else {
+			$prefix = '';
+		}
+
+		return $prefix . $field;
+	}
+
+	public function where($field, $operator, $value) {
+		$field = $this->addFieldPrefix($field);
+
+		$this->where[] = array(
+			'field' => $field,
+			'operator' => $operator,
+			'value' => $value
+		);
+	}
+
+	public function whereGt($field, $value) {
+		$this->where($field, '>', $this->quoteValue($value));
+	}
+
+	public function whereEquals($field, $value) {
+		$this->where($field, '=', $this->quoteValue($value));
+	}
+
+	public function whereNotEquals($field, $value) {
+		$this->where($field, '!=', $this->quoteValue($value));
+	}
+
+	public function whereNotNull($field) {
+		$this->where($field, 'NOT', 'NULL');
+	}
+
+	public function whereSubquery($field, $operator, QueryBuilder $subquery) {
+		$this->where($field, $operator, '(' . $subquery->build() . ')');
+	}
+
+	private function quoteValue($value) {
+		if (is_numeric($value)) {
+			return $value;
+		} else {
+			return '"' . $value . '"';
+		}
+	}
+
+	public function join($tbl) {
+		return $this->leftJoin($tbl);
+	}
+
+	public function leftJoin($tbl) {
+		return $this->joinImpl('LEFT', $tbl);
+	}
+
+	public function joinImpl($direction, $tbl) {
+		$this->joins[$tbl] = array(
+			'direction' => $direction,
+			'table' => $tbl
+		);
+
+		$this->lastJoinedTable = $tbl;
+
+		return $this;
+	}
+
+	public function joinedTable($tbl) {
+		$this->lastJoinedTable = $tbl;
+
+		return $this;
+	}
+
+	public function on($field, $value) {
+		return $this->onImpl($field, '=', $value);
+	}
+
+	public function onGt($field, $value) {
+		return $this->onImpl($field, '>', $value);
+	}
+
+	public function onImpl($field, $operator, $value) {
+		$this->joinConditions[$this->lastJoinedTable][] = array(
+			'field' => $field,
+			'operator' => $operator,
+			'value' => $value
+		);
+
+		return $this;
+	}
+
 	private function buildFields() {
-		return implode(', ', $this->_fields);
+		$clauses = array();
+
+		foreach ($this->fields as $field) {
+			if ($field['alias'] == null) {
+				$clauses[] = $field['field'];
+			} else {
+				$clauses[] = $field['field'] . ' AS ' . $field['alias'];
+			}
+		}
+
+		return implode(', ', $clauses);
 	}
 
 	private function buildOrderBy() {
 		if (empty($this->orderBy)) {
-			return $this->_fields[0];
+			return $this->fields[0]['field'];
 		} else {
 			return implode(', ', $this->orderBy);
 		}
 	}
 
+	private function buildWhere() {
+		$ret = '';
+
+		if (count($this->where) > 0) {
+			$clauses = array();
+
+			foreach ($this->where as $clause) {
+				$clauses[] = $clause['field'] . ' ' . $clause['operator'] . ' ' . $clause['value'];
+			}
+
+			$ret = ' WHERE ' . implode(' AND ', $clauses);
+		}
+
+		return $ret;
+	}
+
+	public function buildJoins() {
+		$ret = '';
+
+		foreach ($this->joins as $join) {
+			$prefix = substr($join['table'], 0, 1);
+
+			$ret .= ' ' . $join['direction'] . ' JOIN ' . $join['table'] . ' ' . $prefix . ' ' . $this->buildJoinConditions($join['table']);
+		}
+		
+		return $ret;		
+	}
+
+	public function buildJoinConditions($joinedTable) {
+		$clauses = array();
+
+		foreach ($this->joinConditions[$joinedTable] as $condition) {
+			$clauses[] = $condition['field'] . ' ' . $condition['operator'] . ' ' . $condition['value'];
+		}
+
+		return 'ON '. implode(' AND ', $clauses);
+	}
+
 	public function build() {
-		return $this->verb . ' ' . $this->buildFields() . ' FROM ' . $this->from . ' ORDER BY ' . $this->buildOrderBy();
+		if (!is_array($this->from)) {
+			throw new \Exception("From table not specified");
+		}
+
+		if (empty($this->fields)) {
+			throw new \Exception("No fields specified");
+		}
+
+		$ret = $this->verb . ' ';
+		$ret .= $this->buildFields() . ' FROM ' . $this->from['table'] . ' ';
+		$ret .= $this->from['prefix'] . $this->buildJoins();
+		$ret .= $this->buildWhere();
+		$ret .= ' ORDER BY ' . $this->buildOrderBy();
+
+		return $ret;
 	}
 }
 
--- src/test/php/QueryBuilderTest.php
+++ src/test/php/QueryBuilderTest.php
@@ -8,17 +8,80 @@ class QueryBuilderTest extends PHPUnit_Framework_TestCase {
 	public function testSelect() {
 		$qb = new QueryBuilder('select');
 		$qb->fields('u.username', 'u.password');
-		$qb->from('users u');
+		$qb->from('users');
 		
 		$this->assertEquals('SELECT u.username, u.password FROM users u ORDER BY u.username', $qb->build());
 	}
 
 	public function testSelectOrderId() {
 		$qb = new QueryBuilder();
-		$qb->from('users u')->fields('u.username');
+		$qb->from('users')->fields('u.username');
 		$qb->orderBy('u.id');
 
 		$this->assertEquals('SELECT u.username FROM users u ORDER BY u.id', $qb->build());
+	}
+
+	public function testAutoPrefixFields() {
+		$qb = new QueryBuilder();
+		$qb->from('users')->fields('id', 'forename');
+		$qb->from('users')->fields('surname');
+		$qb->orderBy('id');
+
+		$this->assertEquals('SELECT u.id, u.forename, u.surname FROM users u ORDER BY u.id', $qb->build());
+	}
+
+	public function testMultipleFilters() {
+		$qb = new QueryBuilder();
+		$qb->from('users');
+		$qb->fields('forename', 'surname');
+		$qb->whereGt('age', 25);
+		$qb->whereEquals('forename', 'bob');
+		$qb->whereEquals('surname', 'exampleton');
+		$qb->whereNotNull('wallet');
+		$qb->orderBy('id');
+
+		$this->assertEquals('SELECT u.forename, u.surname FROM users u WHERE u.age > 25 AND u.forename = "bob" AND u.surname = "exampleton" AND u.wallet NOT NULL ORDER BY u.id', $qb->build());
+	}
+
+	public function testJoin() {
+		$qb = new QueryBuilder();
+		$qb->from('users')->fields('email', array('count(o.id)', 'orderCount'));
+		$qb->leftJoin('orders')->on('o.uid', 'u.id');
+		$qb->orderBy('!orderCount');
+
+		$this->assertEquals('SELECT u.email, count(o.id) AS orderCount FROM users u LEFT JOIN orders o ON o.uid = u.id ORDER BY orderCount', $qb->build());
+	}
+
+	public function testMultiJoin() {
+		$qb = new QueryBuilder();
+		$qb->from('users')->fields('email', array('count(o.id)', 'orderCount'), 'g.title');
+		$qb->leftJoin('orders')->on('o.uid', 'u.id');
+		$qb->leftJoin('groups')->on('u.group', 'g.id');
+		$qb->joinedTable('orders')->onGt('o.date', ':date');
+		$qb->fields('forename', 'surname');
+		$qb->orderBy('email');
+
+		$this->assertEquals('SELECT u.email, count(o.id) AS orderCount, g.title, u.forename, u.surname FROM users u LEFT JOIN orders o ON o.uid = u.id AND o.date > :date LEFT JOIN groups g ON u.group = g.id ORDER BY u.email', $qb->build());
+	}
+
+	public function testWhereNot() {
+		$qb = new QueryBuilder();
+		$qb->from('staff')->fields('id', 'forename', 'surname');
+		$qb->whereNotEquals('karma', 'good');
+
+		$this->assertEquals('SELECT s.id, s.forename, s.surname FROM staff s WHERE s.karma != "good" ORDER BY s.id', $qb->build());
+	}
+
+	public function testSubquery() {
+		$qb = new QueryBuilder();
+		$qb->from('staff');
+		$qb->fields('id', 'forename');
+		$qbInner = new QueryBuilder();
+		$qbInner->from('staff', 's2')->fields('id');
+		$qbInner->whereGt('s2.id', 500);
+		$qb->whereSubquery('s.id', 'NOT IN', $qbInner);
+
+		$this->assertEquals('SELECT s.id, s.forename FROM staff s WHERE s.id NOT IN (SELECT s2.id FROM staff s2 WHERE s2.id > 500 ORDER BY s2.id) ORDER BY s.id', $qb->build());
 	}
 }
 
