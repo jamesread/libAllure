a54866a9b992445c063a6ee6ab9fd7d47fe43146
jamesread <contact@jwread.com>
2016-09-30 10:15:30 +0100

    Added the FilterTracker

---
+++ src/test/php/FilterTrackerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once 'common.php';
+
+require_once 'libAllure/FilterTracker.php';
+
+use \libAllure\FilterTracker;
+
+class FilterTrackerTest extends PHPUnit_Framework_TestCase {
+	public function testConstruct() {
+		$tracker = new FilterTracker();
+
+		$this->assertNotNull($tracker);
+	}
+
+	public function testBooleans() {
+		$tracker = new FilterTracker();
+		$tracker->addBool('test1', 'Test 1');
+
+		$_REQUEST['test1'] = 'asdf';
+
+		$this->assertTrue($tracker->isUsed('test1'));
+		$this->assertTrue($tracker->getValue('test1'));
+	}
+
+	public function testSelect() {
+		$options = array(
+			array(
+				'identifier' => 'bananas',
+				'quantitiy' => 1.4,
+			),
+			array(
+				'identifier' => 'apples',
+				'quantity' => 30
+			),
+			array(
+				'identifier' => 'pears',
+				'quantity' => 3
+			)
+		);
+
+		$tracker = new FilterTracker();
+		$tracker->addSelect('testSelect', $options, 'identifier', 'Test Select');
+
+		$_REQUEST['testSelect'] = 'apples';
+
+		$this->assertTrue($tracker->isUsed('testSelect'));
+		$this->assertEquals($tracker->getValue('testSelect'), 'apples');
+	}
+}
+
+?>
