263817aeac389e19c8099cc9acf9e5d77456cbba
jamesread <contact@jwread.com>
2017-06-28 12:18:18 +0100

    Only look for Smarty if it is not already loaded.

--- src/main/php/libAllure/Template.php
+++ src/main/php/libAllure/Template.php
@@ -21,12 +21,14 @@ namespace libAllure;
 
 if (defined(__FILE__)) { return; } else { define(__FILE__, true); }
 
-if (@include_once 'smarty3/Smarty.class.php') {
-} else if (@include_once 'smarty/Smarty.class.php') {
-} else if (@include_once 'smarty/libs/Smarty.class.php') {
-} else if (@include_once 'Smarty/Smarty.class.php') {
-} else {
-		throw new \Exception('No usable version of Smarty found.');
+if (!class_exists('\Smarty')) {
+	if (@include_once 'smarty3/Smarty.class.php') {
+	} else if (@include_once 'smarty/Smarty.class.php') {
+	} else if (@include_once 'smarty/libs/Smarty.class.php') {
+	} else if (@include_once 'Smarty/Smarty.class.php') {
+	} else {
+			throw new \Exception('No usable version of Smarty found.');
+	}
 }
 
 require_once 'libAllure/Inflector.php';
