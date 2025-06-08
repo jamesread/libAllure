ccafd1a83ca446139f7b29d34825dc956388a73a
jamesread <contact@jread.com>
2025-06-09 00:31:39 +0100

    fix: Dont hardcode textbox size

--- src/main/php/libAllure/ElementTextbox.php
+++ src/main/php/libAllure/ElementTextbox.php
@@ -4,6 +4,9 @@ namespace libAllure;
 
 class ElementTextbox extends Element
 {
+	public $rows = 8;
+	public $cols = 80;
+
     public function render()
     {
         if ($this->value == null) {
@@ -14,6 +17,6 @@ class ElementTextbox extends Element
         $value = stripslashes($value);
         $value = strip_tags($value);
 
-        return sprintf('<textarea id = "%s" name = "%s" rows = "8" cols = "80">%s</textarea>', $this->name, $this->name, $this->value);
+        return sprintf('<textarea id = "%s" name = "%s" rows = "%s" cols = "%s">%s</textarea>', $this->name, $this->name, $this->rows, $this->cols, $this->value);
     }
 }
